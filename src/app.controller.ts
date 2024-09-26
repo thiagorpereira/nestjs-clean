5bbdc06113fcd84a724470ed8aa082d4e5c5da21
Thiago Pereira <thiago.rpereira-ext@grupocasasbahia.com.br>
2024-09-26 17:29:39 -0300

    add: initial clean setup

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from "@nestjs/common";
+import { AppService } from "./app.service";
+
+@Controller("/api")
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get("/teste")
+  what(): string {
+    return this.appService.getHello();
+  }
+
+  @Post("/teste")
+  what2(): string {
+    return "still";
+  }
+}
