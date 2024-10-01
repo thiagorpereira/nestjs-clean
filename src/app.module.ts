57b00a71bfcc65e7bbd35df9cbe90b21ed2dc974
Thiago Pereira <thiago.rpereira-ext@grupocasasbahia.com.br>
2024-09-30 22:58:26 -0300

    add: configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountControler } from './controllers/create-account.controller';
+import { envSchema } from './env';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountControler],
   providers: [PrismaService], // dependências que os controllers possuem
 })
