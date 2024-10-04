d0f127a7edf546c2d4309c36cb0247a382dee984
Thiago Pereira <thiago.rpereira-ext@grupocasasbahia.com.br>
2024-10-04 09:37:30 -0300

    add: auth and questions e2e tests with vite

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -8,8 +8,8 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcryptjs';
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe';
+import { PrismaService } from '@/prisma/prisma.service';
 
 const authenticateBodySchema = z.object({
   email: z.string().email(),
